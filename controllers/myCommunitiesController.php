e7bfdb1ec7d031c0efda6ed32787657f6e7e8518
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-06-22 11:21:12 -0600

    [My Community] + View completed

---
+++ controllers/myCommunitiesController.php
@@ -0,0 +1,27 @@
+<?php
+
+    session_start();
+    if(empty($_SESSION["nameUser"]))
+    {
+        header("Location: ../login.php");
+    }
+
+    //Include database connection model
+    include $_SERVER['DOCUMENT_ROOT'] . '/STAT/models/dbConnection.php';
+
+    $errorMessage = "";
+
+    //Database connection try
+    try
+    {
+        $dbConn = openConn();
+    }
+    //Database exception
+    catch(Exception $e)
+    {
+        $errorMessage = "<div class='alert alert-danger d-grid gap-2 mb-3' role='alert'>" . $e -> getMessage() . "</div>";
+    }
+
+    $userId = $_SESSION["userId"];
+
+?>
