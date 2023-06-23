4b39fa05953c02e90bdac30a16ed3f5738972db3
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-06-22 18:52:25 -0600

    [Community] + Edit functionality completed

---
+++ controllers/communityController.php
@@ -0,0 +1,96 @@
+<?php 
+
+    //Include database connection controller
+    include $_SERVER['DOCUMENT_ROOT'] . '/STAT/models/dbConnection.php';
+
+    //Initialize error message
+    $errorMessage = "";
+
+    //Get id community
+    $idCommunity = $_GET["idCommunity"];
+    $idUser = $_SESSION["userId"];
+
+    $isEditUser = false;
+    $commTitle = "Hola";
+
+    //Database connection try
+    try
+    {
+        $dbConn = openConn();
+
+        //Sql try | Get all values from exam to edit
+        try
+        {
+            $sql = $dbConn -> query("select c.* from communities as c inner join userscommunities as uc on c.idCommunity = uc.idCommunity where uc.idCommunity = '$idCommunity' and uc.idUser = '$idUser'");
+
+            if($sql -> num_rows > 0)
+            {
+                $isEditUser = true;
+            }
+
+            $sqlGeneral = $dbConn -> query("select * from communities where idCommunity = '$idCommunity'");
+
+            if($sqlGeneral -> num_rows > 0)
+            {
+                $commData = $sqlGeneral -> fetch_object();
+
+                $commTitle = $commData -> title; 
+                $commAuthor = $commData -> author;
+                $commCountry = $commData -> country;
+                $commLanguage = $commData -> lang;
+                $commDescription = $commData -> description;  
+            }
+        }
+        //Sql exception
+        catch(Exception $e)
+        {
+            $errorMessage = "<div class='alert alert-danger d-grid gap-2 mb-3' role='alert'>" . $e -> getMessage() . "</div>";
+        }
+    }
+    //Database exception
+    catch(Exception $e)
+    {
+        $errorMessage = "<div class='alert alert-danger d-grid gap-2 mb-3' role='alert'>" . $e -> getMessage() . "</div>";
+    }
+
+    //If form is sent through post method
+    if ($_SERVER["REQUEST_METHOD"] === "POST") 
+    {
+        //If button is clicked
+        if(isset($_POST['btnSaveCommunity'])) 
+        {
+            //Get all post values 
+            $commTitle = $_POST["title"];
+            $commAuthor = $_POST["author"];
+            $commCountry = $_POST["country"];
+            $commLanguage = $_POST["language"];
+            $commDescription = $_POST["description"];
+
+            //Sql try
+            try
+            {
+                //TO-DO check duplicated communities
+
+                $sql = "update communities set title ='$commTitle', author='$commAuthor', country='$commCountry', lang='$commLanguage', description='$commDescription' where idCommunity='$idCommunity'";
+
+                if($dbConn -> query($sql) === TRUE)
+                {
+
+                    closeConn($dbConn);
+                    header("Location: ../views/myCommunities.php");
+                }
+                else{
+                    closeConn($dbConn);
+                    $errorMessage = "<div class='alert alert-alert d-grid gap-2 mb-3' role='alert'>Couldn't save Community</div>";
+                }
+            }
+            //Sql exception
+            catch(Exception $e)
+            {
+                $errorMessage = "<div class='alert alert-alert d-grid gap-2 mb-3' role='alert'>" . $e -> getMessage() . "</div>";
+            }
+
+        }
+
+    }
+?>
---
+++ views/community.php
@@ -0,0 +1,163 @@
+<?php
+    session_start();
+    if(empty($_SESSION["nameUser"]))
+    {
+        header("Location: ../login.php");
+    }
+
+    include '../controllers/communityController.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Communities</title>
+
+        <link rel="stylesheet" href="../resources/vendors/bootstrap/bootstrap.min.css">
+
+        <link rel="stylesheet" href="../resources/css/style.css">
+    </head>
+
+    <body>
+
+        <header>
+            <?php
+                include "../utils/navbar.php";
+            ?>
+        </header>
+
+
+        <div class="wrapperContainer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-6">
+                        <h1>Community - <?php echo $commTitle;?></h1>
+                    </div>
+                </div>
+
+                <?php 
+                    if($isEditUser)
+                    {
+                        ?>
+                        <div class="row mt-4">
+                            <form method="post" action="" enctype="multipart/form-data">
+                                <div class="row mt-4">
+                                    <div class="col-lg-6 col-md-6 mb-3">
+                                        <label for="title" class="form-label">Community Title</label>
+                                        <input type="text" class="form-control" id="title" required name="title" value="<?php echo $commTitle;?>">
+                                    </div>
+                                    <div class="col-lg-6 col-md-6 mb-3">
+                                        <label for="author" class="form-label">Author</label>
+                                        <input type="text" class="form-control" id="author" required name="author" value="<?php echo $_SESSION["nameUser"];?>">
+                                    </div>
+                                </div>
+
+                                <div class="row mt-2">
+                                    <div class="col-lg-4 col-md-4 mb-3">
+                                        <label for="country" class="form-label">Country</label>
+                                        <select class="form-select" id="country" aria-label="Default select example" name="country">
+                                            <option value="Mexico" <?php if ($commCountry == "Mexico") echo "selected"; ?>>Mexico</option>
+                                            <option value="Canada" <?php if ($commCountry == "Canada") echo "selected"; ?>>Canada</option>
+                                            <option value="France" <?php if ($commCountry == "France") echo "selected"; ?>>France</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-lg-4 col-md-4 mb-3">
+                                        <label for="language" class="form-label">Language</label>
+                                        <select class="form-select" id="language" aria-label="Default select example" name="language">
+                                            <option value="Spanish" <?php if ($commLanguage == "Spanish") echo "selected"; ?>>Spanish</option>
+                                            <option value="English" <?php if ($commLanguage == "English") echo "selected"; ?>>English</option>
+                                            <option value="French" <?php if ($commLanguage == "French") echo "selected"; ?>>French</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="row mt-2">
+                                    <div class="col-lg-12 col-md-12 mb-3">
+                                        <label for="examDescription" class="form-label">Description</label>
+                                        <textarea class="form-control" id="examDescription" rows="3" required name="description"><?php echo $commDescription?></textarea>
+                                    </div>
+                                </div>
+
+                                <?php echo $errorMessage; ?>
+
+                                <div class="row mt-4 d-flex justify-content-end">
+                                    <div class="col-lg-3 col-md-3 mb-3 d-flex justify-content-center">
+                                        <button name="btnSaveCommunity" class="btn btn-md btn-primary col-lg-12" type="submit" id="btnSave">Save Changes</button> 
+                                    </div>
+                                </div>
+
+                                
+                            </form>
+                        </div>
+                    <?php    
+                    }
+                ?>
+
+                <?php 
+                    if(!$isEditUser)
+                    {
+                        ?>
+                        <div class="row mt-4">
+                            <h4 class="mt-3"><?php echo $commDescription;?></h4>
+                            <h6 style="color: grey;">Author: <?php echo $commAuthor?> | <?php echo $commCountry?> | <?php echo $commLanguage?> </h6>
+                        </div>
+                    <?php    
+                    }
+                ?>
+
+
+                
+
+                <div class="row mt-5">
+                    <div class="col-lg-12 col-md-6 d-flex">
+                        <!-- <table class="table">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Author</th>
+                                <th scope="col">Year</th>
+                                <th scope="col">School</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <?php
+
+                                    $registerCounter = 0;
+
+                                    try
+                                    {
+                                        $sql = mysqli_query($dbConn, "select c.* from communities as c inner join userscommunities as uc on c.idCommunity = uc.idCommunity where uc.idUser = '$idUser'");
+
+                                        while($communityData = mysqli_fetch_assoc($sql)):
+                                        
+                                            $registerCounter++;
+                                        ?>
+                                            <tr>
+                                                <th scope="row"><?php echo $registerCounter;?></th>
+                                                <td><a href="../views/community.php?idCommunity=<?php echo $communityData["idCommunity"]?>" style="color: black;"><strong><?php echo $communityData["title"];?></strong></a></td>
+                                                <td><?php echo $communityData["description"];?></td>
+                                                <td><?php echo $communityData["author"];?></td>
+                                            </tr>
+
+
+                                    <?php endwhile;    
+                                    }
+                                    catch(Exception $e)
+                                    {
+                                        $errorMessage = "<div class='alert alert-danger d-grid gap-2 mb-3' role='alert'>" . $e -> getMessage() . "</div>";
+                                    }
+                                ?>
+                                    
+                            </tbody>
+                        </table> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="../resources/vendors/bootstrap/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
